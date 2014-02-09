e3bd79359cfe21f35d7e73ae1b8086c3f8047256
Matevz Mihalic <matevz.mihalic@gmail.com>
2014-02-09 14:39:57 +0100

    First try.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ AutoRetweet
 ===========
 
 Retweet script written in python, because we can.
+
+Installation
+======
+
+* Install python, supervisor and pip
+* Install virtualenv `sudo pip install virtualenv`
+* Clone this repository `git clone https://github.com/lucasgrzegorczyk/AutoRetweet.git`
+* cd into AutoRetweet folder and create virtualenv `virtualenv --no-site-packages retweetenv`
+* Activate virtualenv `source retweetenv/bin/activate`
+* Install pip requirements `pip install -r requirements.txt`
+* Copy settingy.py.example to settings.py and edit it
+* Run retweet for the first time `python retweet.py`
+* Follow instructions and copy oath keys to settings
+* Copy retweet_supervisor.conf to your supervisor conf folder (probably /etc/supervisor/conf.d) and fix paths in it
+* Reload supervisor
---
+++ retweet.py
@@ -0,0 +1,39 @@
+from twython import Twython
+from twython import TwythonStreamer
+
+import settings
+
+class RetweetingStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'retweeted_status' in data:
+            my_retweets = [item for item in twitter.get_retweets(id=data['retweeted_status']['id']) if item["user"]['id'] == my_user_id]
+        else:
+            my_retweets = []
+        if len(my_retweets) == 0 and data['user']['screen_name'] in settings.ACCOUNTS:
+            twitter.retweet(id=data['id'])
+            print "Retweeting %s: %s" % (data['user']['name'].encode('utf-8'), data.get('text', "Tweet ID: " + str(data['id'])).encode('utf-8'))
+
+    def on_error(self, status_code, data):
+        print status_code, data
+
+if not hasattr(settings, 'OAUTH_TOKEN'):
+    twitter = Twython(settings.APP_KEY, settings.APP_SECRET)
+    auth = twitter.get_authentication_tokens()
+
+    print "Open %s to authorize application and get PIN  number." % auth['auth_url']
+    twitter = Twython(settings.APP_KEY, settings.APP_SECRET, auth['oauth_token'], auth['oauth_token_secret'])
+
+    pin = raw_input("Enter PIN: ")
+    final_step = twitter.get_authorized_tokens(pin)
+
+    print "Now save tokens to your settings.py and run this script again.\n"
+    print "OAUTH_TOKEN = \"%s\"" % final_step['oauth_token']
+    print "OAUTH_TOKEN_SECRET = \"%s\"\n" % final_step['oauth_token_secret']
+
+else:
+    twitter = Twython(settings.APP_KEY, settings.APP_SECRET, settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+    user_ids = [str(user['id']) for user in twitter.lookup_user(screen_name=','.join(settings.ACCOUNTS))]
+    my_user_id = twitter.verify_credentials()['id']
+
+    stream = RetweetingStreamer(settings.APP_KEY, settings.APP_SECRET, settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+    stream.statuses.filter(follow=user_ids)
